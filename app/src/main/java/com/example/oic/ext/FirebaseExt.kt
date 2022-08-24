69a6a84acef7adb6b38316067c02afa625105c9d
Parkduksung <duksung1234@naver.com>
2022-08-24 22:43:21 +0900

    [feature] FirebaseExt.kt 추가.

---
+++ app/src/main/java/com/example/oic/ext/FirebaseExt.kt
@@ -0,0 +1,15 @@
+package com.example.oic.ext
+
+import com.example.oic.data.repo.FirebaseRepository
+
+
+fun FirebaseRepository.loginUser(
+    email: String,
+    password: String,
+    callback: (isSuccess: Boolean) -> Unit
+) {
+    getFirebaseAuth().signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener {
+            callback(it.isSuccessful)
+        }
+}
