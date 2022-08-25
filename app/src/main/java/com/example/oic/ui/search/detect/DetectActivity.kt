11ce45b565ec1573550d5dafd4bbc5ba130ac070
Parkduksung <duksung1234@naver.com>
2022-08-25 18:39:25 +0900

    [feature] DetectActivity 등 추가. (카메라 프리뷰에 물체감지 되게끔 추가, 퍼미션 확인)

---
+++ app/src/main/java/com/example/oic/ui/search/detect/DetectActivity.kt
@@ -0,0 +1,9 @@
+package com.example.oic.ui.search.detect
+
+import com.example.oic.R
+import com.example.oic.base.BaseActivity
+import com.example.oic.databinding.ActivityDetectBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetectActivity : BaseActivity<ActivityDetectBinding>(R.layout.activity_detect)
